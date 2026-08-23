71d89af2424ff019bd9081346e3c52ee205ff042
aditya1212-29 <aditya05450@gmail.com>
2026-08-24 02:04:07 +0530

    Time: 0 ms (100%), Space: 95 MB (79.95%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    double solve(vector<int>& nums1, vector<int>& nums2, int val){
+        int low = 0, high = nums1.size();
+        int n = nums1.size(), m = nums2.size();
+        double ans = 0;
+        while(low <= high){
+            int mid1 = (low + high) / 2;
+            int mid2 = val - mid1;
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+            if(mid1 < n) r1 = nums1[mid1];
+            if(mid2 < m) r2 = nums2[mid2];
+            if(mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            if(mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+            if(l1 <= r2 && l2 <= r1){
+                if((n + m) % 2 == 1){
+                    ans = max(l1, l2);
+                    return ans;
+                }
+                else{
+                    ans = max(l1, l2) + min(r1, r2);
+                    break;
+                }
+            }
+            else if(l2 > r1 ){
+                low = mid1 + 1;
+            }
+            else{
+                high = mid1 - 1;
+            }
+        }    
+        return ans / 2.0;
+    }
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size(), m = nums2.size();
+        int val = (n + m + 1) / 2;
+        if(n < m){
+            return solve(nums1, nums2, val);
+        }
+        return solve(nums2, nums1, val);
+    }
+};
