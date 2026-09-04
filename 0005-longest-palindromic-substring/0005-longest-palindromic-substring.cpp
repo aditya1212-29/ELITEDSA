ace5ba9980ac7a7d167667f26765431ef92f8df3
aditya1212-29 <aditya05450@gmail.com>
2026-09-04 15:47:23 +0530

    Time: 833 ms (9.96%), Space: 292 MB (6.52%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -20,9 +20,10 @@ public:
             return dp[i][j] = 0;
         }
         if(dp[i][j] != -1) return dp[i][j];
-        bool a = solve(i, j - 1, s, dp, start, len);
-        bool b = solve(i + 1, j ,s, dp, start, len);
-        if(s[i] == s[j] && solve(i + 1, j - 1, s, dp, start, len)){
+        solve(i + 1, j - 1, s, dp, start, len);
+        solve(i, j - 1, s, dp, start, len);
+        solve(i + 1, j ,s, dp, start, len);
+        if(s[i] == s[j] && dp[i + 1][j - 1]){
             int nlen = j - i + 1;
             if(nlen > len){
                 len = nlen;
@@ -36,7 +37,7 @@ public:
         int n = s.size();
         if(s == "") return "";
         vector<vector<int>> dp(n, vector<int>(n, -1));
-        int start = -1, len = -1;
+        int start = 0, len = 1;
         bool ans = solve(0, n - 1, s, dp, start, len);
         return s.substr(start, len);
     }
