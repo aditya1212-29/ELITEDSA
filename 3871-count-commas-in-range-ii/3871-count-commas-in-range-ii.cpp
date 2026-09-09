2cbca7de15f38078feaece1d01cbba43596c7f23
aditya1212-29 <aditya05450@gmail.com>
2026-09-09 16:26:17 +0530

    Time: 0 ms (100%), Space: 9.1 MB (19.2%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
-    long long countCommas(long long number) {
-        vector<int>vec;
-        string s = to_string(number);
-        int n = s.size();
-        int i = n;
-        while(i > 0){
-        int start = max(0, i - 3);
-        string num = s.substr(start, i - start);
-        vec.push_back(stoi(num));
-        i -= 3;
-        }
-        for(int i = 0; i < vec.size(); i++){
-            cout<<vec[i]<<endl;
-        }
-        long long ans = 0;
-        long long mul = 1LL * 1000;
-        for(int j = 1; j < vec.size(); j++){
-            long long val = number - (mul - 1);
+    long long countCommas(long long n) {
+        long long ans = 0, mul = 1LL * 1000;
+        while(mul <= n){
+            long long val = n -(mul - 1);
             ans += val;
+            if(mul > LLONG_MAX / 1000)
+            break;
             mul *= 1000;
         }
         return ans;
