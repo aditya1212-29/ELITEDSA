ea9f55ac8e56cd2de196f59fb51a004696a67a31
aditya1212-29 <aditya05450@gmail.com>
2026-09-12 01:09:00 +0530

    Time: 0 ms (100%), Space: 8.7 MB (6.5%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int solve(int i, int j, int n){
+    int solve(int i, int j, int n, vector<vector<int>> &dp){
         if(i > j) return 1;
         if(i == j) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
         int ans = 0;
         for(int index = i ; index <= j ; index++){
-            int left = solve(i, index - 1, n);
-            int right = solve(index + 1, j, n);
+            int left = solve(i, index - 1, n, dp);
+            int right = solve(index + 1, j, n, dp);
             ans += left * right;
         }
-        return ans;
+        return dp[i][j] = ans;
     }
     int numTrees(int n) {
-        return solve(1, n, n);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        return solve(1, n, n, dp);
     }
 };
