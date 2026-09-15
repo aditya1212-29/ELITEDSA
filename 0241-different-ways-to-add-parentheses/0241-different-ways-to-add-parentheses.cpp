93c710e9d6dbdc2623a657cb58d59de6ab5ff04d
aditya1212-29 <aditya05450@gmail.com>
2026-09-15 11:39:41 +0530

    Time: 4 ms (29.92%), Space: 14.1 MB (42.65%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -1,52 +1,35 @@
 class Solution {
 public:
-    vector<int> solve(string &expression, int i, int j){
-        if(i == j){
-            return {expression[i] - '0'};
-        }
-        if(i + 1 == j){
-            return {stoi(expression.substr(i, 2))};
-        }
-        vector<int>vec;
-        for(int index = i ; index <= j; index++){
-            if(expression[index] == '+' || expression[index] == '-'||
-                expression[index] == '*'|| expression[index] == '/'){
-                vector<int>left = solve(expression, i, index - 1);
-                vector<int>right = solve(expression, index + 1, j);
-                if(expression[index] == '+'){
-                    for(int k = 0; k < left.size(); k++){
-                        for(int l = 0; l < right.size(); l++){
-                            vec.push_back(left[k] + right[l]);
-                        }
+    vector<int>solve(string &exp, int i, int j){
+        if(i == j || i + 1 == j) return {stoi(exp.substr(i, j - i + 1))};
+        vector<int>ans;
+        vector<int>left, right;
+        for(int index = i; index <= j; index++){
+            if(exp[index] == '+' || exp[index] == '-' || exp[index] == '*' || exp[index] == '/'){
+                left = solve(exp, i, index - 1);
+                right = solve(exp, index + 1, j);
+            }
+            for(int k = 0; k < left.size(); k++){
+                for(int l = 0; l < right.size(); l++){
+                    if(exp[index] == '+'){
+                        ans.push_back(left[k] + right[l]);
                     }
-                }
-                if(expression[index] == '-'){
-                    for(int k = 0; k < left.size(); k++){
-                        for(int l = 0; l < right.size(); l++){
-                            vec.push_back(left[k] - right[l]);
-                        }
+                    if(exp[index] == '-'){
+                        ans.push_back(left[k] - right[l]);
                     }
-                }
-                if(expression[index] == '*'){
-                    for(int k = 0; k < left.size(); k++){
-                        for(int l = 0; l < right.size(); l++){
-                            vec.push_back(left[k] * right[l]);
-                        }
+                    if(exp[index] == '*'){
+                        ans.push_back(left[k] * right[l]);
                     }
-                }
-                if(expression[index] == '/'){
-                    for(int k = 0; k < left.size(); k++){
-                        for(int l = 0; l < right.size(); l++){
-                            vec.push_back(left[k] / right[l]);
-                        }
+                    if(exp[index] == '/'){
+                        ans.push_back(left[k] / right[l]);
                     }
                 }
             }
         }
-        return vec;
+        return ans;
     }
     vector<int> diffWaysToCompute(string expression) {
-        int n = expression.size() - 1;
-        return solve(expression, 0, n);
+        int n = expression.size();
+        return solve(expression, 0, n - 1);
     }
 };
