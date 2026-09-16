86c822e7ffcbc3b9cc9c11c3c7d17f4cced75f6c
aditya1212-29 <aditya05450@gmail.com>
2026-09-16 20:17:33 +0530

    Time: 0 ms (100%), Space: 8.7 MB (59.58%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    int solve(int i, int j, string &s, vector<vector<int>> &pali){
-        if(i > j) return 0;
-        int ans = 0;
-        for(int index = i; index <= j; index++){
-            bool check = pali[i][index];
-            if(check){
-                ans += 1;
-            }
-        }
-        return ans + solve(i + 1, j, s, pali);
-    }
     int countSubstrings(string s) {
         int n = s.size();
-        vector<vector<int>> pali(n, vector<int>(n, 0));
-        for(int i = n - 1; i >= 0; i--){
-            for(int j = i; j < n; j++){
-                if(s[i] == s[j] && (j - i <= 2 || pali[i + 1][j - 1]))
-                pali[i][j] = 1;
+        int l, r, ans = 0;
+        for(int i = 0; i < n; i++){
+            l = r = i;
+            while(l >= 0 && r < n){
+                if(s[l--] == s[r++]) ans++;
+                else break;
             }
         }
-        return  solve(0, n - 1, s, pali);
+        for(int i = 0; i < n; i++){
+            l = i;
+            r = i + 1;
+            while(l >= 0 && r < n){
+                if(s[l--] == s[r++]) ans++;
+                else break;
+            }
+        }
+        return ans;
     }
 };
