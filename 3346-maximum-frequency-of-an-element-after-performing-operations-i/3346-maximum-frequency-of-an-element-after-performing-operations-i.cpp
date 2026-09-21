18c8ef825044bfec319705ff984b47873f786508
aditya1212-29 <aditya05450@gmail.com>
2026-09-22 02:03:44 +0530

    Time: 283 ms (39.52%), Space: 200.7 MB (23.34%) - LeetHub

--- 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
-        int maxi = *max_element(nums.begin(), nums.end());
-        int mini = *min_element(nums.begin(), nums.end());
-        int s = maxi + k - (mini - k) + 2;
-        int offset = mini - k;
-        int n = nums.size();
-        unordered_map<int, int>mp;
-        for(auto it : nums)
-        mp[it]++;
+        int maxi =*max_element(nums.begin(), nums.end()) + k;
+        int s = maxi + 2;
+        unordered_map<int, int> mp;
+        for(auto it : nums) mp[it]++;
         vector<int>vec(s, 0);
-        for(int i = 0; i < n; i++){
-            int start = nums[i] - k - offset;
-            int end = nums[i] + k - offset;
-            vec[start] += 1;
-            if(end + 1 < s)
-            vec[end + 1] -= 1;
+        for(int i = 0; i < nums.size(); i++){
+            int l = max(nums[i] - k, 0);
+            int r = min(nums[i] + k, maxi);
+            vec[l] += 1;
+            vec[r + 1] -= 1;
         }
         for(int i = 1; i < s; i++){
             vec[i] += vec[i - 1];
         }
         int ans = INT_MIN;
         for(int i = 0; i < s; i++){
-            int cnt = vec[i];
-            int index = i + offset;
-            int occ = mp[index];
-            int req = cnt - occ;
-            int take = occ + min(req, numOperations);
-            ans = max(ans, take);
+            int cnt = mp[i];
+            int occ = vec[i];
+            int req = occ - cnt;
+            ans = max(ans, cnt + min(req, numOperations));
         }
         return ans;
     }
